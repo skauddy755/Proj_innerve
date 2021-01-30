87327ccde7cb92d0a24491d8fcc06743a3cb4846
Chetan Pandey <chetanpandey1266@gmail.com>
2021-01-31 00:46:46 +0530

    inference files2

--- smart-clinic/app.py
+++ smart-clinic/app.py
@@ -6,12 +6,20 @@ from PIL import Image
 from utils import img2heatmap
 from alzheimer.classification.classification import predict
 from aptos.inference_aptos import predict_aptos
+from melanoma.inference_melanoma import predict_melanoma
+from pneumonia.inference_pneumonia import predict_pneumonia
+from breast_cancer.inference_breast import predict_breast
 
 app = Flask(__name__)
 
+app.config["PNEUMONIA_IMAGE_UPLOADS"] = os.getcwd()+"/pneumonia/input_imgs"
+app.config["BREAST_CANCER_UPLOADS"] = os.getcwd()+"/breast_cancer/input_imgs"
+app.config["MELANOMA_UPLOADS"] = os.getcwd()+"/melanoma/input_img"
+app.config["APTOS_UPLOADS"] = os.getcwd()+"/aptos/input_imgs"
 app.config["ALZHEIMER_IMAGE_UPLOADS"] = os.getcwd()+"/alzheimer/input_imgs/"
-app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG"]
 app.config["ALZHEIMER_IMAGE_HEATMAP"] = os.getcwd()+"/alzheimer/heat_map/"
+app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG"]
+
 
 
 @app.route('/')
@@ -56,15 +64,69 @@ def alzheimersdetection():
     return render_template('alzheimer.html', predicted = False)
 
 
-@app.route('/services/breast')
+@app.route('/services/breast', methods=['POST', 'GET'])
 def breast():
+    if request.method == 'POST':
+        if request.files:
+            image = request.files["image"]
+            if(image.filename == ''):
+                return "NO FILE UPLOADED"
+            filename = image.filename 
+            name, ext = filename.split('.')
 
-    return "<h1>Working On it</h1>"
+            if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
+                image.save(os.path.join(app.config["BREAST_CANCER_UPLOADS"], filename))
+                result = predict_breast(os.path.join(app.config["BREAST_CANCER_UPLOADS"], filename))
+                return render_template('breast.html', predicted=True, supplied_text = result)
+    return render_template('breast.html', predicted=False)
 
 @app.route('/services/melanoma', methods=['POST', 'GET'])
 def melanoma():
-    return render_template("melanoma.html", predicted=False )
+    if request.method == 'POST':
+        if request.files:
+            image = request.files["image"]
+            if(image.filename == ''):
+                return "NO FILE UPLOADED"
+            filename = image.filename 
+            name, ext = filename.split('.')
 
+            if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
+                image.save(os.path.join(app.config["MELANOMA_UPLOADS"], filename))
+                result = predict_melanoma(os.path.join(app.config["MELANOMA_UPLOADS"], filename))
+                return render_template('melanoma.html', predicted=True, supplied_text = result)
+    return render_template("melanoma.html", predicted=False)
+
+@app.route('/services/pneumonia', methods=['POST', 'GET'])
+def pneumonia():
+    if request.method == 'POST':
+        if request.files:
+            image = request.files["image"]
+            if(image.filename == ''):
+                return "NO FILE UPLOADED"
+            filename = image.filename 
+            name, ext = filename.split('.')
+
+            if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
+                image.save(os.path.join(app.config["PNEUMONIA_UPLOADS"], filename))
+                result = predict_pneumonia(os.path.join(app.config["PNEUMONIA_UPLOADS"], filename))
+                return render_template('pneunomia.html', predicted=True, supplied_text = result)
+    return render_template('pneumonia.html', predicted=False)
+
+@app.route('/services/aptos', methods=['POST', 'GET'])
+def aptos():
+    if request.method == 'POST':
+        if request.files:
+            image = request.files["image"]
+            if(image.filename == ''):
+                return "NO FILE UPLOADED"
+            filename = image.filename 
+            name, ext = filename.split('.')
+
+            if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
+                image.save(os.path.join(app.config["APTOS_UPLOADS"], filename))
+                result = predict_aptos(os.path.join(app.config["APTOS_UPLOADS"], filename))
+                return render_template('aptos.html', predicted=True, supplied_text = result)
+    return render_template('aptos.html', predicted = False)
 
 @app.route('/blog')
 def blog():
